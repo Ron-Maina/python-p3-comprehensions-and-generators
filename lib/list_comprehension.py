a71c25660d482400dcd61d2f14f1bb9f096d85b8
Ron Maina <ronmwangi00@gmail.com>
2023-08-19 14:53:54 +0300

    Completed Lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    list_even = [num for num in num_list if (num % 2  == 0)]
+    return list_even
     pass
 
 def make_exclamation(sentence_list):
+    list_string = [f"{string}!" for string in sentence_list]
+    return list_string
     pass
